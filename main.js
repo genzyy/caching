0dc6f8b672147fb2ba1e89f634e08a0b0ed6bbe9
Rishit Pandey <rishpandey8097@gmail.com>
2021-04-16 19:52:19 +0530

    converted to js

--- main.js
+++ main.js
@@ -1,89 +1,49 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var _this = this;
-var express = require('express');
-var axios = require('axios');
-var redis = require('redis');
-var app = express();
-var url = 'https://jobs.github.com/positions.json?search=node.js';
-var redisPort = 6379;
-var client = redis.createClient(redisPort);
-client.on('error', function (error) {
-    console.log(error);
+// Express backend framework.
+const express = require('express');
+// axios for getting data from the api.
+const axios = require('axios');
+// redis for saving the cache on the user machine.
+const redis = require('redis');
+const app = express();
+const url = 'https://jobs.github.com/positions.json?search=node.js';
+// redis uses port number 6379 as the default port.
+const redisPort = 6379;
+const client = redis.createClient(redisPort);
+
+// if redis encounters an error then it is printed in the console.
+client.on('error', error => {
+  console.log(error);
 });
-app.get('/jobs', function (req, res) { return __awaiter(_this, void 0, void 0, function () {
-    var query;
-    var _this = this;
-    return __generator(this, function (_a) {
-        query = req.query.search;
-        try {
-            client.get(query, function (error, jobs) { return __awaiter(_this, void 0, void 0, function () {
-                var jobs_1;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0:
-                            if (error)
-                                throw error;
-                            if (!jobs) return [3 /*break*/, 1];
-                            res.status(200).send({
-                                jobs: JSON.parse(jobs),
-                                message: 'data received from cache stored on the machine!'
-                            });
-                            return [3 /*break*/, 3];
-                        case 1: return [4 /*yield*/, axios.get("https://jobs.github.com/positions.json?search=" + query)];
-                        case 2:
-                            jobs_1 = _a.sent();
-                            client.setex(query, 600, JSON.stringify(jobs_1.data));
-                            res.status(200).send({
-                                jobs: jobs_1.data,
-                                message: 'cache miss'
-                            });
-                            _a.label = 3;
-                        case 3: return [2 /*return*/];
-                    }
-                });
-            }); });
-        }
-        catch (error) {
-            res.status(500).send(error);
-        }
-        return [2 /*return*/];
+
+app.get('/jobs', async (req, res) => {
+  const query = 'node.js';
+  try {
+    // check whether the given call is cached in the memory or not.
+    client.get(query, async (error, jobs) => {
+      if (error) throw error;
+
+      if (jobs) {
+        res.status(200).send({
+          jobs: JSON.parse(jobs),
+          message: 'data received from cache stored on the machine!'
+        });
+      } else {
+        const jobs = await axios.get(
+          `https://jobs.github.com/positions.json?search=${query}`
+        );
+        // if redis doesnt have the data then it gets stored as cache.
+        client.setex(query, 600, JSON.stringify(jobs.data));
+        res.status(200).send({
+          jobs: jobs.data,
+          message: 'cache miss'
+        });
+      }
     });
-}); });
-app.listen(3000, function () {
-    console.log('Server started running...');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server started running...');
 });
